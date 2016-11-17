aa7b8d4433c1b781f57011935b26a32c4670443c
Maxim Drob <drobmax1995@gmail.com>
2016-11-17 22:38:31 +0300

    update gitignore

--- src/ads_board/dal/db_conf.clj
+++
@@ -1,7 +0,0 @@
-(ns ads-board.dal.db-conf)
-
-(def db-spec {:subprotocol "mysql"
-               :subname "//localhost:3306/ad_db"
-               :user "root"
-               :password "1111"
-               :zeroDateTimeBehavior "convertToNull"})
